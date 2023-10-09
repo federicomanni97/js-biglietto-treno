d3b7da5635e120910690dab3a73bf808d3cbcfdb
federicomanni97 <manni_federico@libero.it>
2023-10-09 15:45:24 +0200

    javascript-done

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+let km = prompt("Inserisci i km");
+console.log(km)
+let età = prompt("Inserisci la tua età");
+console.log(età)
+const price = 0.21;
+const twentyDiscount = 20/100;
+const fortyDiscount = 40/100;
+let travelCost = ( km * price);
+let fullPrice
+
+if (età < 18) {
+    fullPrice = travelcost - (travelCost * twentyDiscount); 
+} else if (età >= 65){
+    fullPrice = travelCost - (travelCost * fortyDiscount);
+} else {
+    fullPrice = travelCost;
+}
+
+console.log(fullPrice.toFixed(2));
